0b680aa89eb7f80bf09e5332e65343d8479ac299
MDShakibul <sakib6610@gmail.com.com>
2022-04-05 19:45:04 +0600

    Chart view done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>Blogs</h1>
+        <div className='m-3'>
+            <h5>Question 1: Describe context api.</h5>
+            <p>Answer: We can pass value by props. If we need to send a value on another component then we use props. But sometime we need to send value via more components then send value next component then next component this is way to go at the destination component that call props drilling. But we can send data by useing context api. For context api, set up context api in parent component and use this component value direct on child component</p>
+
+            <h5>Question 1: Describe context api.</h5>
+
         </div>
     );
 };
---
+++ src/components/Dashboard/Chart/Chart.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Line,
+  LineChart,
+  Pie,
+  PieChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+const Chart = () => {
+  const [charts, setCharts] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setCharts(data));
+  }, []);
+
+  return (
+    <div className="row">
+      <div className="col-lg-6 ">
+        <h1 className="text-center">Line Chart</h1>
+        <div className="d-flex justify-content-center">
+        <LineChart width={400} height={400} data={charts}>
+        <Line dataKey={"sell"}></Line>
+        <XAxis dataKey={"month"} />
+        <YAxis></YAxis>
+        <CartesianGrid></CartesianGrid>
+        <Tooltip></Tooltip>
+      </LineChart>
+        </div>
+      </div>
+      <div className="col-lg-6">
+        <h1 className="text-center">Bar Chart</h1>
+
+        <div className="d-flex justify-content-center">
+        <BarChart width={400} height={400} data={charts}>
+        <Bar dataKey={"revenue"} fill="#8884d8"></Bar>
+        <XAxis dataKey={"investment"} />
+        <YAxis></YAxis>
+        <Tooltip></Tooltip>
+      </BarChart>
+        </div>
+      </div>
+      <div>
+      <h1 className='text-center'>Pie Chart</h1>
+
+        <div className="d-flex justify-content-center">
+        <PieChart width={400} height={400}>
+          <Pie
+            data={charts}
+            dataKey={"sell"}
+            cx="50%"
+            cy="50%"
+            outerRadius={100}
+            fill="#8884d8"
+          />
+
+          <Tooltip></Tooltip>
+        </PieChart>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chart;
